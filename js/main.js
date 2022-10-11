3db5d3bfed9c7a2de840ee7e7ad9a1181e3f2dcc
sergentors705 <sergentors705@gmail.com>
2022-10-11 15:12:10 +0600

    splited main.js into modules

--- js/main.js
+++ js/main.js
@@ -1,43 +1,4 @@
-function getRandomNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if (min < 0 || max < 0 || min === max) {
-    return NaN;
-  } else if (min > max) {
-    return Math.floor(Math.random() * (min - max + 1)) + max;
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+import {createDescription} from './data.js';
 
-function createIdGenerator () {
-  let lastGeneratedId = 0;
-
-  return function () {
-    lastGeneratedId += 1;
-    return Number(lastGeneratedId);
-  };
-}
-
-function calcStringLength(string, maxLength) {
-  if (string.length >= +maxLength) {
-    return false;
-  }
-  return true;
-}
-
-calcStringLength('sdhjfdshffsdjfkhdskfhsdjfkskd', 20);
-
-const orderedId = createIdGenerator();
-const orderedUrl = createIdGenerator();
-
-const createDescription = () => ({
-  id: orderedId(),
-  url: `photos/${orderedUrl()}.jpg`,
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  likes: getRandomNumber(15, 200),
-  comments: getRandomNumber(0, 200),
-});
-
-
-const createPhotoDescriptions = Array.from({length: 25}, createDescription);
+const createPhotoDescriptions = () => Array.from({length: 25}, createDescription);
 createPhotoDescriptions();
---
+++ js/util.js
@@ -0,0 +1,31 @@
+const getRandomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min < 0 || max < 0 || min === max) {
+    return NaN;
+  } else if (min > max) {
+    return Math.floor(Math.random() * (min - max + 1)) + max;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return function () {
+    lastGeneratedId += 1;
+    return Number(lastGeneratedId);
+  };
+};
+
+const calcStringLength = (string, maxLength) => {
+  if (string.length >= +maxLength) {
+    return false;
+  }
+  return true;
+};
+
+calcStringLength('sdhjfdshffsdjfkhdskfhsdjfkskd', 20);
+
+export {getRandomNumber};
+export {createIdGenerator};
